590457544ead1656042e3fdbd82cd3e5d972319b
MISA <ravololonantenainamisa7@gmail.com>
2024-08-06 08:57:42 +0300

    fix deploy problems

--- django_todo_project/todo_project/settings.py
+++ django_todo_project/todo_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,8 +26,8 @@ SECRET_KEY = 'django-insecure-0y#!689ne8+if2$wc$b!m1^z!uzma@_(4==z)w^xgu!=i3*0*&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Application definition
 
