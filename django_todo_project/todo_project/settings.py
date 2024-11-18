363ff5527f501b9c935ad3568215d4b07b1d14ca
Misa Ravololonantenaina <55345000+Misa100@users.noreply.github.com>
2024-11-18 14:12:16 +0300

    feat : configuration liaison react et django par docker

--- django_todo_project/todo_project/settings.py
+++ django_todo_project/todo_project/settings.py
@@ -132,4 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://todo-app-react-django.vercel.app",
+    "https://remarkable-magic-production-c110.up.railway.app"
 ]
